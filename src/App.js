5d9e2b799c7858d77dad0efd63fd0646b2c47a29
an3-bit <andrewmogambi12@gmail.com>
2024-12-27 11:17:12 +0300

    Added the category page

--- src/App.js
+++ src/App.js
@@ -1,45 +1,37 @@
-
 import './App.css';
 import { Navbar } from './Components/Navbar/Navbar';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ShopCategory } from './Pages/ShopCategory';
 import { Product } from './Pages/Product';
 import { Cart } from './Pages/Cart';
 import { LoginSignup } from './Pages/LoginSignup';
-import { Hero } from './Components/Hero/Hero';
-import { Item } from './Components/Item/Item';
-import {Popular} from './Components/Popular/Popular';
-import Offers from './Components/Offers/Offers';
-import NewCollections from './Components/NewCollections/NewCollections';
-import NewsLetter from './Components/NewsLetter/NewsLetter';
+import Shop from './Pages/shop';
 import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png';
+import women_banner from './Components/Assets/banner_women.png';
+import kid_banner from './Components/Assets/banner_kids.png';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<shop/>}/>
-        <Route path='/mens' element={<ShopCategory category="men"/>}/>
-        <Route path='/womens' element={<ShopCategory category="women"/>}/>
-        <Route path='/kids' element={<ShopCategory category="kid"/>}/>
-        <Route path='/product' element={<Product/>}>
-          <Route path=':productId' element={<Product/>}/>
-        </Route>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/login' element={<LoginSignup/>}/>
+        <Navbar />
+        <Routes>
+          {/* Home Page */}
+          <Route path="/" element={<Shop />} />
+
+          {/* Other Pages */}
+          <Route path="/mens" element={<ShopCategory banner={men_banner} category="men" />} />
+          <Route path="/womens" element={<ShopCategory banner={women_banner} category="women" />} />
+          <Route path="/kids" element={<ShopCategory banner={kid_banner} category="kid" />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
         </Routes>
-        
-        <Hero/>
-      <Item/>
-      <Popular/>
-      <Offers/>
-      <NewCollections/>
-      <NewsLetter/>
-      <Footer/>
+
+        {/* Footer for all pages */}
+        <Footer />
       </BrowserRouter>
-      
     </div>
   );
 }
