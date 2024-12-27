8a3e706f0d6973bbf3cf03b727f8d24b7fed32b5
an3-bit <andrewmogambi12@gmail.com>
2024-12-27 11:18:20 +0300

    changes a few things on a popopular css

--- src/Pages/shop.jsx
+++ src/Pages/shop.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import { Hero } from '../Components/Hero/Hero'
-import Popular from '../Components/Popular/Popular'
-import Offers from '../Components/Offers/Offers'
-import NewCollections from '../Components/NewCollections/NewCollections'
-import NewsLetter from '../Components/NewsLetter/NewsLetter'
+import React from 'react';
+import { Hero } from '../Components/Hero/Hero';
+import Popular from '../Components/Popular/Popular';
+import Offers from '../Components/Offers/Offers';
+import NewCollections from '../Components/NewCollections/NewCollections';
+import NewsLetter from '../Components/NewsLetter/NewsLetter';
+import { Item } from '../Components/Item/Item';
 
-export const shop = () => {
+const Shop = () => {
   return (
     <div>
-        <Hero/>
-        <Popular/>
-        <Offers/>
-        <NewCollections/>
-        <NewsLetter/>
+      <Hero />
+      <Item/>
+      <Popular />
+      <Offers />
+      <NewCollections />
+      <NewsLetter />
     </div>
-  )
-}
+  );
+};
+
+export default Shop;
